afd16e75cf53fb71d6452de87e151670b8c92fba
nemoryoliver <nemoryoliver@gmail.com>
2022-09-28 09:34:24 +0800

    early commits

---
+++ supabase/functions/upload-vault/index.ts
@@ -0,0 +1,19 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+
+console.log("Hello from Functions!")
+
+serve(async (req) => {
+  const { name } = await req.json()
+  const data = {
+    message: `Hello ${name}!`,
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
